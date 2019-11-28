00cd0c081d872d5112bff4346287c0db48177ab2
Huszar Beata <beata.huszar@adevinta.com>
2019-11-28 21:21:25 +0100

    Initialising the Context object

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component {
+  // specific naming for context system
+  static contextType = LanguageContext;
+
   render() {
     return <button className="ui button primary">Submit</button>;
   }
