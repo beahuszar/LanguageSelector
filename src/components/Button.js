9e4fca184da04d8efceb02d2d41d1c768dc7f3b9
Huszar Beata <beata.huszar@adevinta.com>
2019-11-28 22:06:33 +0100

    Accessing data with Consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,19 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-  // specific naming for context system
-  static contextType = LanguageContext;
+  renderSubmitText(value) {
+    return value === 'english' ? 'Submit' : 'Elkuld'
+  };
 
   render() {
-    const text = this.context === 'english' ? 'Submit' : 'Elkuld'
-    return <button className="ui button primary">{text}</button>;
+    return (
+      <button className="ui button primary">
+        <LanguageContext.Consumer>
+          {/** This function will be called by Consumer with the value inside the Pipe */}
+          {(value) => this.renderSubmitText(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
   }
 }
 
